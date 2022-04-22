e15e912c74a924914d7cd842134c5cbe9bd796d0
Adir Elmakais <adirelmakais@Adirs-Mac.local>
2022-04-22 16:03:04 +0300

    initial commit

---
+++ war-challenge/war_challengeApp.swift
@@ -0,0 +1,17 @@
+//
+//  war_challengeApp.swift
+//  war-challenge
+//
+//  Created by Adir Elmakais on 20/04/2022.
+//
+
+import SwiftUI
+
+@main
+struct war_challengeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
